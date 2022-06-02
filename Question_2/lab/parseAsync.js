d1a95ca2372deac4c0c069ed16a6546a48dfe1ac
saulo <saulocamposi@gmail.com>
2022-06-02 06:19:54 +0100

    Update -- beta proj test

--- Question_2/lab/parseAsync.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const data = fs.readFileSync('parse.csv', 'utf8')
-console.log(data);
-
-
--- Question_2/restaurant_timetable.js
+++ Question_2/restaurant_timetable.js
@@ -9,37 +9,92 @@ const setupDb = () => {
  });
 }
 
-const dropDb = () => {}
-
-const createTables = () =>{
-  let sql_restaurant_table = `CREATE TABLE restaurants(id INTEGER PRIMARY KEY, restaurant, timetable)`;  
-   db.run(sql_restaurant_table);
-  
+const dropDb = () => {
+  let sql_restaurant_drop_table = `DROP TABLE restaurants`;
+  db.run(sql_restaurant_drop_table);  
 }
 
-const parseData =  () => {    
+const createTables = () =>{
+  let sql_restaurant_table = `CREATE TABLE restaurants(
+                              id INTEGER PRIMARY KEY, 
+                              restaurant, 
+                              days,
+                              time,
+                              numberDays,
+                              hourTimeBegin INTEGER,
+                              hourTimeEnd INTEGER,
+                              minTimeBegin INTEGER,
+                              minTimeEnd INTEGER,
+                              originalTime)`;  
+   db.run(sql_restaurant_table);  
+}
 
-    let sql_restaurant_insert = `INSERT INTO restaurants(restaurant,timetable) VALUES (?,?)`;
+const freshDb = () => {
+  setupDb();
+  dropDb();
+  createTables();
+}
+
+const parseData =  () => {
+  let sql_restaurant_insert = `INSERT INTO restaurants(
+                                restaurant,
+                                days,
+                                time,
+                                numberDays,
+                                hourTimeBegin,
+                                hourTimeEnd,
+                                minTimeBegin,
+                                minTimeEnd,
+                                originalTime) VALUES (?,?,?,?,?,?,?,?,?)`;
     let csvJson = parseCSV();
 
-    csvJson.forEach( element => {      
-      db.run(sql_restaurant_insert,[element.restaurant, element.timetable],(err) =>{
-        if(err) return console.log(err.message);
-      });
+    csvJson.forEach( restaurants => {      
+      restaurants.timetable.forEach( timetable => {
+        let hourBegin = timetable.numberTimes[0].hour;
+        let hourEnd   = timetable.numberTimes[1].hour;
+
+        let minBegin = timetable.numberTimes[0].min;
+        let minEnd   = timetable.numberTimes[1].min;
+
+        db.run(sql_restaurant_insert,
+          [restaurants.restaurant,
+            timetable.days,
+            timetable.time,
+            timetable.numberDays,
+            hourBegin,
+            hourEnd,
+            minBegin,
+            minEnd,
+            timetable.originalTime],(err) =>{
+          if(err) return console.log(err.message);
+        });
+      });     
     });
    
 }
-
-const searchOpenRestaurants = function (date) {
-     sql = `SELECT * FROM restaurants`;
+const listAll = () => {
+  sql = `SELECT * FROM restaurants`;
      db.all(sql, [] , (err,rows) => {
          if(err) return console.log(err.message);
          rows.forEach((row) => { console.log(row); });
      });
 }
+const searchOpenRestaurants = function (date) {
+      let day = date.getDay();
+      let hour = date.getHours();
+      let min = date.getMinutes();
+      sql = ` SELECT * FROM restaurants 
+              where numberDays LIKE '%' || ? || '%'
+              AND hourTimeBegin <= ? AND minTimeBegin <= ? 
+              AND hourTimeEnd >= ? AND minTimeEnd <= ?` ;
+      db.all(sql, [day,hour,min,hour,min] , (err,rows) => {
+          if(err) return console.log(err.message);
+          rows.forEach((row) => { console.log(row); });
+      });
+}
 
 const parseCSV = () => {
-  let file = fs.readFileSync('parse.csv', 'utf8');
+  let file = fs.readFileSync('rest_open_hours.csv', 'utf8');
   let dataInLines = file.split(/\r?\n/);
   let json = jsonSlice(dataInLines);
   return json;
@@ -51,7 +106,7 @@ const jsonSlice = (dataInLines) => {
   dataInLines.forEach((element,index) => {
       dataInLines[index] = {      
            restaurant : sliceRestaurants(element),     
-           timetable : time(element)
+           timetable : parseTimetable(time(element))
           };
        });
        return dataInLines;
@@ -60,10 +115,11 @@ const jsonSlice = (dataInLines) => {
 const weekdays = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
 const weekdaysObj = {'Mon': 1,'Tue' : 2 ,'Wed' : 3,'Thu':4,'Fri':5,'Sat' :6 ,'Sun':0 };
 const parseTimetable = (time) => {
+  time = time.replaceAll('"','')
+  let originalTime = time;
   let a = time.split("/");
   a.forEach((element,index) => {
       let e = element.trim();
-      e = e.replace('"','') 
       let days =  e.slice(0,e.indexOf(' '));
       let time = e.slice(e.indexOf(' '));
       time = time.trim();
@@ -72,31 +128,12 @@ const parseTimetable = (time) => {
         time = timereplaced.str;
         days = days.concat(timereplaced.usedElement);
       };
-      days = days.replace(',','-');
       let numberDays = daysInNumber(days,weekdaysObj);
-      console.log(numberDays);
-      
-
-      
-
-      a[index] =   { days : days, time : time } ;
+      let numberTimes = timeInNumber(time);
+      a[index] =   { days : days, time : time, numberDays: numberDays, numberTimes: numberTimes,originalTime: originalTime } ;
   });
   return a;
 }
-
-// weekdays.some(element => {
-//   if (times.includes(element)) {
-//     return true;
-//   }
-//   return false;
-// });
-
-// const contains = weekdays.some(element => {
-//   if (str.includes(element)) {
-//     return true;
-//   }
-//   return false;
-// });
 
 const containsAndReplace = (str,arr,pattern) => {
   let elem = ""
@@ -110,29 +147,88 @@ const containsAndReplace = (str,arr,pattern) => {
    return elem;
 };
 
+const range = (start, end, length = end - start + 1) =>
+  Array.from({ length }, (_, i) => start + i)
+
 const daysInNumber = (days,obj) => {
-  days = days.split("-");
-  console.log(days);
+  if (days.includes(",")) {
+    days = days.split(",");  
+  }else{
+    days = days.split("-");
+  }
+  
   days.forEach((element,index) => {
-    days[index] = obj.element
+    if (element.includes("-")) {
+      element = element.split("-");
+      days.push(...range(obj[element[0]],obj[element[1]]))      
+    }else{
+      days[index] = obj[element];
+    }    
   });
+  days = filterList(days);
    return days;
 };
 
+const filterList = (l) => {
+  return l.filter(x => typeof x === "number");
+}
 
-let tx = '"Mon-Thu, Sun 11:30 am - 10 pm  / Fri-Sat 11:30 am - 11 pm"';
-let txa = 'Sun 11:30 am - 10 pm';
-//console.log(parseTimetable(tx));
-parseTimetable(tx)
-//console.log(contains(txa,weekdays)); 
+const timeInNumber = (time) => {
+  time = time.split("-");
+    
+  time.forEach((element,index) => {
+    element = element.trim();
+    if(element.includes('am')){
+      element = element.replace('am','');
+      element = timeSplit(element,0);
+    }else{
+      element = element.replace('pm');
+      element = element.trim();
+      element = timeSplit(element,1);
+    }
+    time[index] = element;
+  })
+  return time;
+}
 
-//console.log(JSON.stringify(parseCSV()));
+const timeSplit = (element,am) => {
+  let timesplit = 0;
+  let hour = 0;
+  let min = 0;
 
-//1console.log(parseCSV());
+  if (element.includes(":")) {
+    element = element.split(":");
+    hour = element[0];
+    min = parseInt(element[1],10);
+    hour = parseInt(hour,10);
+    hour = (am == 1) ? hour + 12 : hour;
+    timesplit = {hour: hour, min: min};
+  }else{        
+    hour = element = parseInt(element,10);
+    hour = (am == 1) ? hour + 12 : hour;
+    timesplit = {hour: hour, min: min};
+  }
+  return timesplit;
+}
 
-//setupDb();
+/*************************************/
+/* Instruction for Run the App
+/*********************************** */
+//Use setupDb in all steps
+setupDb();
+
+//Command to dropDb
+//dropDb();
+
+//Command to createTables
 //createTables();
 
+//Command to insert data from CSV to Db
 //parseData();
-//a = new Date('June 19, 2022 23:15:30')
-//searchOpenRestaurants(a);
+
+//Command to list all data from Db
+//listAll();
+
+//Command to list time available
+//bookTable = new Date("June 19, 2022 23:15:30")
+//searchOpenRestaurants(bookTable);
